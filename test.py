f1b6e230766756bae23bcfc5681606826fb53b86
TaiseiYamana <mit.yamatail@gmail.com>
2021-06-14 07:07:20 +0000

    a

--- test.py
+++ test.py
@@ -4,3 +4,6 @@ architecture_names = sorted(
         name for name in models.__dict__
         if name.islower() and not name.startswith("__")
         and callable(models.__dict__[name]))
+
+
+print('\n'.join(architecture_names))
