1c254211eb37b5964fe361b82cf19b93ffc4bf13
TaiseiYamana <mit.yamatail@gmail.com>
2021-06-15 15:06:55 +0000

    update

--- source/pretrain_teacher_model.py
+++ source/pretrain_teacher_model.py
@@ -1,47 +1,126 @@
-# import library
+# Distillation zoo
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+import os
+import sys
+import time
+import logging
+import argparse
+import numpy as np
+from itertools import chain
 
+# dalib
+import random
+import time
+import warnings
+import sys
+import argparse
+import shutil
+import os.path as osp
+
+import torch
+import torch.nn as nn
+import torch.backends.cudnn as cudnn
+from torch.optim import SGD
+from torch.optim.lr_scheduler import LambdaLR
+from torch.utils.data import DataLoader
+import torchvision.transforms as T
+import torch.nn.functional as F
 
 sys.path.append('..')
+from utils import AverageMeter, accuracy, transform_time
+from utils import load_pretrained_model, save_checkpoint
+from utils import create_exp_dir, count_parameters_in_MB
+
+from dalib.adaptation.mcc import MinimumClassConfusionLoss, ImageClassifier
+import common.vision.datasets as datasets
+import common.vision.models as models
+#from common.vision.transforms import ResizeImage
+from common.utils.data import ForeverDataIterator
+#from common.utils.metric import accuracy, ConfusionMatrix
+#from common.utils.meter import AverageMeter, ProgressMeter
+#from common.utils.logger import CompleteLogger
+#from common.utils.analysis import collect_feature, tsne, a_distance
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def main(args):
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    if args.cuda:
+		    torch.cuda.manual_seed(args.seed)
+		    cudnn.enabled = True
+		    cudnn.benchmark = True
+    logging.info("args = %s", args)
 
 
-architecture_names = sorted(
+if __name__ == '__main__':
+    architecture_names = sorted(
         name for name in models.__dict__
         if name.islower() and not name.startswith("__")
-        and callable(models.__dict__[name]))
-dataset_names = sorted(
-        name for name in datasets.__dict__
-        if not name.startswith("__") and callable(datasets.__dict__[name]))
-
-
-# define parser
-parser = argparse.ArgumentParser(description='pretrain Teacher net')
-
-# dataset & domain
-dataset_names = sorted(
+        and callable(models.__dict__[name])
+    )
+    dataset_names = sorted(
         name for name in datasets.__dict__
         if not name.startswith("__") and callable(datasets.__dict__[name])
     )
-parser.add_argument('--dataset', metavar='DATA', default='Office31',
+
+    parser = argparse.ArgumentParser(description='pretrain Teacher net')
+    # root path
+    parser.add_argument('--save_root', type=str, default='./results', help='models and logs are saved here')
+    parser.add_argument('--img_root', type=str, default='./datasets', help='path name of image dataset')
+    parser.add_argument('--note', type=str, default='pt_of31_A2W_r50', help='note for this run') #office31_source_pretrain
+    # dataset parameters
+    parser.add_argument('-d', '--dataset', metavar='DATA', default='Office31',
                         help='dataset: ' + ' | '.join(dataset_names) +
                              ' (default: Office31)')
-parser.add_argument('--source', default = 'A', help='source domain(s)')
-#parser.add_argument('--target', default = 'W', help='target domain(s)')
-
-# network
+    parser.add_argument('-s', '--source', help='source domain(s)')
+    parser.add_argument('-t', '--target', help='target domain(s)')
+    # model parameters
     parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet18',
                         choices=architecture_names,
                         help='backbone architecture: ' +
                              ' | '.join(architecture_names) +
                              ' (default: resnet18)')
+    parser.add_argument('--bottleneck-dim', default=256, type=int,
+                        help='Dimension of bottleneck')
+    parser.add_argument('--temperature', default=2.0, type=float, help='parameter temperature scaling')
+    parser.add_argument('--trade-off', default=1., type=float,
+                        help='the trade-off hyper-parameter for transfer loss')
+    # training parameters
+    parser.add_argument('-b', '--batch-size', default=64, type=int, help='mini-batch size (default: 64)')
+    parser.add_argument('--lr', '--learning-rate', default=0.1, type=float, help='initial learning rate')
+    parser.add_argument('--lr-gamma', default=0.0003, type=float, help='parameter for lr scheduler')
+    parser.add_argument('--lr-decay', default=0.75, type=float, help='parameter for lr scheduler')
+    parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
+    parser.add_argument('--wd', '--weight-decay', default=1e-3, type=float, help='weight decay (default: 1e-3)')
+    parser.add_argument('-j', '--workers', default=4, type=int, help='number of data loading workers (default: 4)')
+    parser.add_argument('--epochs', default=10, type=int, help='number of total epochs to run')
+    parser.add_argument('-i', '--iters-per-epoch', default=500, type=int, help='Number of iterations per epoch')
+    parser.add_argument('-p', '--print-freq', default=50, type=int, help='print frequency (default: 50)')
+    parser.add_argument('--seed', default=1, type=int, help='seed for initializing training. ')
+    parser.add_argument("--phase", type=str, default='train', choices=['train', 'test', 'analysis'],
+                        help="When phase is 'test', only test the model."
+                             "When phase is 'analysis', only analysis the model.")
+    args = parser.parse_args()
 
-# various path
-parser.add_argument('--save_root', type=str, default='./results/Teacher', help='models and logs are saved here')
-parser.add_argument('--img_root', type=str, default='./datasets/DA', help='path name of image dataset')
+    args.save_root = os.path.join(args.save_root, args.note)#./results/DA/pt_of31_A_r50
+    args.img_root = os.path.join(args.img_root, args.dataset)#./datasets/DA/Office31
+    create_exp_dir(args.save_root)
 
-args, unparsed = parser.parse_known_args()
+    log_format = '%(message)s'
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=log_format)
+    fh = logging.FileHandler(os.path.join(args.save_root, 'log.txt'))
+    fh.setFormatter(logging.Formatter(log_format))
+    logging.getLogger().addHandler(fh)
 
-# main code
-def main():
+    main(args)
 
-if __name__ == '__main__':
-	main()
+
+
+
+
+
+
+
