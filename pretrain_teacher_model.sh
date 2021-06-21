9e2a2bb90992b75b4f97f646d33758e2b16b84a8
山名　泰生 <54575368+TaiseiYamana@users.noreply.github.com>
2021-06-21 22:46:44 +0900

    Create pretrain_teacher_model.sh

---
+++ pretrain_teacher_model.sh
@@ -0,0 +1,73 @@
+# Office31
+CUDA_VISIBLE_DEVICES=0 python3 pretrain_teacher_model.py --save_root '/content/drive/MyDrive/results/PT' \
+                                                         --img_root '/content' \
+                                                         --note 'pt_of31_A2W_r50' \
+                                                         -a 'resnet50' \
+                                                         --bottleneck-dim 1024 \
+                                                         -d Office31 \
+                                                         -s A \
+                                                         -t W \
+                                                         --epochs 20 \
+                                                         -i 500 \
+                                                         --seed 2 \
+                                                         --temperature 2.5
+CUDA_VISIBLE_DEVICES=0 python3 pretrain_teacher_model.py --save_root '/content/drive/MyDrive/results/PT' \
+                                                         --img_root '/content' \
+                                                         --note 'pt_of31_A2D_r50' \
+                                                         -a 'resnet50' \
+                                                         --bottleneck-dim 1024 \
+                                                         -d Office31 \
+                                                         -s A \
+                                                         -t D \
+                                                         --epochs 20 \
+                                                         -i 500 \
+                                                         --seed 2 \
+                                                         --temperature 2.5
+CUDA_VISIBLE_DEVICES=0 python3 pretrain_teacher_model.py --save_root '/content/drive/MyDrive/results/PT' \
+                                                         --img_root '/content' \
+                                                         --note 'pt_of31_W2A_r50' \
+                                                         -a 'resnet50' \
+                                                         --bottleneck-dim 1024 \
+                                                         -d Office31 \
+                                                         -s W \
+                                                         -t A \
+                                                         --epochs 20 \
+                                                         -i 500 \
+                                                         --seed 2 \
+                                                         --temperature 2.5
+CUDA_VISIBLE_DEVICES=0 python3 pretrain_teacher_model.py --save_root '/content/drive/MyDrive/results/PT' \
+                                                         --img_root '/content' \
+                                                         --note 'pt_of31_W2D_r50' \
+                                                         -a 'resnet50' \
+                                                         --bottleneck-dim 1024 \
+                                                         -d Office31 \
+                                                         -s W \
+                                                         -t D \
+                                                         --epochs 20 \
+                                                         -i 500 \
+                                                         --seed 2 \
+                                                         --temperature 2.5
+CUDA_VISIBLE_DEVICES=0 python3 pretrain_teacher_model.py --save_root '/content/drive/MyDrive/results/PT' \
+                                                         --img_root '/content' \
+                                                         --note 'pt_of31_D2A_r50' \
+                                                         -a 'resnet50' \
+                                                         --bottleneck-dim 1024 \
+                                                         -d Office31 \
+                                                         -s D \
+                                                         -t A \
+                                                         --epochs 20 \
+                                                         -i 500 \
+                                                         --seed 2 \
+                                                         --temperature 2.5
+CUDA_VISIBLE_DEVICES=0 python3 pretrain_teacher_model.py --save_root '/content/drive/MyDrive/results/PT' \
+                                                         --img_root '/content' \
+                                                         --note 'pt_of31_D2W_r50' \
+                                                         -a 'resnet50' \
+                                                         --bottleneck-dim 1024 \
+                                                         -d Office31 \
+                                                         -s D \
+                                                         -t W \
+                                                         --epochs 20 \
+                                                         -i 500 \
+                                                         --seed 2 \
+                                                         --temperature 2.5
