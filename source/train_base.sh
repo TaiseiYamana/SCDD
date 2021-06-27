ab58d304803b8164f0a727ea42a7b5706eafde3a
山名　泰生 <54575368+TaiseiYamana@users.noreply.github.com>
2021-06-28 03:01:08 +0900

    Update train_base.sh

--- source/train_base.sh
+++ source/train_base.sh
@@ -1,7 +1,7 @@
 # Office31
 # resnet18
 # A→W
-!CUDA_VISIBLE_DEVICES=0 python3 train_base.py --save_root '/content/drive/MyDrive/results/Base' \
+CUDA_VISIBLE_DEVICES=0 python3 train_base.py --save_root '/content/drive/MyDrive/results/Base' \
                                       --img_root '/content' \
                                       --note 'base_of31_A2W_r18' \
                                       -a 'resnet18' \
@@ -12,7 +12,7 @@
                                       -i 500 \
                                       --seed 2
 # A→D
-!CUDA_VISIBLE_DEVICES=0 python3 train_base.py --save_root '/content/drive/MyDrive/results/Base' \
+CUDA_VISIBLE_DEVICES=0 python3 train_base.py --save_root '/content/drive/MyDrive/results/Base' \
                                       --img_root '/content' \
                                       --note 'base_of31_A2D_r18' \
                                       -a 'resnet18' \
@@ -23,7 +23,7 @@
                                       -i 500 \
                                       --seed 2
 # W→A
-!CUDA_VISIBLE_DEVICES=0 python3 train_base.py --save_root '/content/drive/MyDrive/results/Base' \
+CUDA_VISIBLE_DEVICES=0 python3 train_base.py --save_root '/content/drive/MyDrive/results/Base' \
                                       --img_root '/content' \
                                       --note 'base_of31_W2A_r18' \
                                       -a 'resnet18' \
@@ -34,7 +34,7 @@
                                       -i 500 \
                                       --seed 2
 # W→D
-!CUDA_VISIBLE_DEVICES=0 python3 train_base.py --save_root '/content/drive/MyDrive/results/Base' \
+CUDA_VISIBLE_DEVICES=0 python3 train_base.py --save_root '/content/drive/MyDrive/results/Base' \
                                       --img_root '/content' \
                                       --note 'base_of31_W2D_r18' \
                                       -a 'resnet18' \
@@ -45,7 +45,7 @@
                                       -i 500 \
                                       --seed 2              
 # D→A
-!CUDA_VISIBLE_DEVICES=0 python3 train_base.py --save_root '/content/drive/MyDrive/results/Base' \
+CUDA_VISIBLE_DEVICES=0 python3 train_base.py --save_root '/content/drive/MyDrive/results/Base' \
                                       --img_root '/content' \
                                       --note 'base_of31_D2A_r18' \
                                       -a 'resnet18' \
@@ -56,7 +56,7 @@
                                       -i 500 \
                                       --seed 2                      
 # D→W
-!CUDA_VISIBLE_DEVICES=0 python3 train_base.py --save_root '/content/drive/MyDrive/results/Base' \
+CUDA_VISIBLE_DEVICES=0 python3 train_base.py --save_root '/content/drive/MyDrive/results/Base' \
                                       --img_root '/content' \
                                       --note 'base_of31_D2W_r18' \
                                       -a 'resnet18' \
