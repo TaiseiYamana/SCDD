ad1b6865b7eb9fd5da350bfef56bb038e3942d1b
TaiseiYamana <mit.yamatail@gmail.com>
2021-07-01 17:35:39 +0000

    update

--- dalib/adaptation/dcoral.py
+++ dalib/adaptation/dcoral.py
@@ -12,22 +12,22 @@ class DeepCoralLoss(nn.Module):
         """
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-        n = logits.size(0)  # batch_size
+        n = self.logits.size(0)  # batch_size
 
         id_row = torch.ones(n).resize(1, n).to(device=device)
-        sum_column = torch.mm(id_row, logits)
+        sum_column = torch.mm(id_row, self.logits)
         mean_column = torch.div(sum_column, n)
         term_mul_2 = torch.mm(mean_column.t(), mean_column)
-        d_t_d = torch.mm(logits.t(), logits)
+        d_t_d = torch.mm(self.logits.t(), self.logits)
         c = torch.add(d_t_d, (-1 * term_mul_2)) * 1 / (n - 1)
 
         return c        
 
     def forward(self, source_logits: torch.Tensor, target_logits: torch.Tensor) -> torch.Tensor:
-        d = source_logits.size(1) # num_classes
+        d = self.source_logits.size(1) # num_classes
         
-        source_c = self.compute_convariance(source_logits)
-        target_c = self.compute_convariance(target_logits)
+        source_c = self.compute_convariance(self.source_logits)
+        target_c = self.compute_convariance(self.target_logits)
 
         dcoral_loss = torch.sum(torch.mul((source_c - target_c), (source_c - target_c)))
         dcoral_loss = dcoral_loss / (4 * d * d)
