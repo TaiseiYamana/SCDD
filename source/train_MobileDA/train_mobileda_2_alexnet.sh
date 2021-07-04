c6a0fcfb3bfd8716adbd5bda23d6d0980e55479d
山名　泰生 <54575368+TaiseiYamana@users.noreply.github.com>
2021-07-04 22:48:53 +0900

    Update train_mobileda_2_alexnet.sh

--- source/train_MobileDA/train_mobileda_2_alexnet.sh
+++ source/train_MobileDA/train_mobileda_2_alexnet.sh
@@ -13,28 +13,15 @@ CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_2_alex.py --save_root '/content/dr
                                       -i 500 \
                                       --seed 2 \
                                       --temp 2.0
-# A→D                                      
+# D→W                                      
 CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_2_alex.py --save_root '/content/drive/MyDrive/results/MobileDA/r34_2_alex' \
                                       --img_root '/content' \
-                                      --note 'of31_A2D' \
+                                      --note 'of31_D2W' \
                                       --t_arch 'resnet34' \
-                                      --t-model-param '/content/drive/MyDrive/results/Base/r34/of31_A2D/model_best.pth.tar' \
+                                      --t-model-param '/content/drive/MyDrive/results/Base/r34/of31_D2W/model_best.pth.tar' \
                                       -d Office31 \
-                                      -s A \
-                                      -t D \
-                                      --epochs 20 \
-                                      -i 500 \
-                                      --seed 2 \
-                                      --temp 2.0
-# W→A                                      
-CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_2_alex.py --save_root '/content/drive/MyDrive/results/MobileDA/r34_2_alex' \
-                                      --img_root '/content' \
-                                      --note 'of31_W2A' \
-                                      --t_arch 'resnet34' \
-                                      --t-model-param '/content/drive/MyDrive/results/Base/r34/of31_W2A/model_best.pth.tar' \
-                                      -d Office31 \
-                                      -s W \
-                                      -t A \
+                                      -s D \
+                                      -t W \
                                       --epochs 20 \
                                       -i 500 \
                                       --seed 2 \
@@ -47,6 +34,19 @@ CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_2_alex.py --save_root '/content/dr
                                       --t-model-param '/content/drive/MyDrive/results/Base/r34/of31_W2D/model_best.pth.tar' \
                                       -d Office31 \
                                       -s W \
+                                      -t D \
+                                      --epochs 20 \
+                                      -i 500 \
+                                      --seed 2 \
+                                      --temp 2.0                                      
+# A→D                                      
+CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_2_alex.py --save_root '/content/drive/MyDrive/results/MobileDA/r34_2_alex' \
+                                      --img_root '/content' \
+                                      --note 'of31_A2D' \
+                                      --t_arch 'resnet34' \
+                                      --t-model-param '/content/drive/MyDrive/results/Base/r34/of31_A2D/model_best.pth.tar' \
+                                      -d Office31 \
+                                      -s A \
                                       -t D \
                                       --epochs 20 \
                                       -i 500 \
@@ -64,17 +64,17 @@ CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_2_alex.py --save_root '/content/dr
                                       --epochs 20 \
                                       -i 500 \
                                       --seed 2 \
-                                      --temp 2.0
-# D→W                                      
+                                      --temp 2.0                                      
+# W→A                                      
 CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_2_alex.py --save_root '/content/drive/MyDrive/results/MobileDA/r34_2_alex' \
                                       --img_root '/content' \
-                                      --note 'of31_D2W' \
+                                      --note 'of31_W2A' \
                                       --t_arch 'resnet34' \
-                                      --t-model-param '/content/drive/MyDrive/results/Base/r34/of31_D2W/model_best.pth.tar' \
+                                      --t-model-param '/content/drive/MyDrive/results/Base/r34/of31_W2A/model_best.pth.tar' \
                                       -d Office31 \
-                                      -s D \
-                                      -t W \
+                                      -s W \
+                                      -t A \
                                       --epochs 20 \
                                       -i 500 \
                                       --seed 2 \
-                                      --temp 2.0 
+                                      --temp 2.0
