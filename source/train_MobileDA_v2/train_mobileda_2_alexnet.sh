f63c528f50b9b261d3b509620d0d989f0ad80c5f
山名　泰生 <54575368+TaiseiYamana@users.noreply.github.com>
2021-07-04 22:07:16 +0900

    Create train_mobileda_2_alexnet.sh

---
+++ source/train_MobileDA_v2/train_mobileda_2_alexnet.sh
@@ -0,0 +1,92 @@
+# office-31
+# resnet34→arexnet
+# A→W
+CUDA_VISIBLE_DEVICES=0 python3 Distillation_from_teacher_alex.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r34_2_arex' \
+                                      --img_root '/content' \
+                                      --note 'of31_A2W' \
+                                      --t_arch 'resnet34' \
+                                      --t-bottleneck-dim 512 \
+                                      --t-model-param '/content/drive/MyDrive/results/PT/MCC/r34/of31_A2W' \
+                                      -d Office31 \
+                                      -s A \
+                                      -t W \
+                                      --epochs 20 \
+                                      -i 500 \
+                                      --seed 2 \
+                                      --mcc_temp 2.5 \
+                                      --st_temp 4.0
+# D→W
+CUDA_VISIBLE_DEVICES=0 python3 Distillation_from_teacher_alex.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r34_2_arex' \
+                                      --img_root '/content' \
+                                      --note 'of31_D2W' \
+                                      --t_arch 'resnet34' \
+                                      --t-bottleneck-dim 512 \
+                                      --t-model-param '/content/drive/MyDrive/results/PT/MCC/r34/of31_D2W' \
+                                      -d Office31 \
+                                      -s D \
+                                      -t W \
+                                      --epochs 20 \
+                                      -i 500 \
+                                      --seed 2 \
+                                      --mcc_temp 2.5 \
+                                      --st_temp 4.0 
+# W→D
+CUDA_VISIBLE_DEVICES=0 python3 Distillation_from_teacher_alex.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r34_2_arex' \
+                                      --img_root '/content' \
+                                      --note 'of31_W2D' \
+                                      --t_arch 'resnet34' \
+                                      --t-bottleneck-dim 512 \
+                                      --t-model-param '/content/drive/MyDrive/results/PT/MCC/r34/of31_W2D' \
+                                      -d Office31 \
+                                      -s w \
+                                      -t D \
+                                      --epochs 20 \
+                                      -i 500 \
+                                      --seed 2 \
+                                      --mcc_temp 2.5 \
+                                      --st_temp 4.0      
+# A→D
+CUDA_VISIBLE_DEVICES=0 python3 Distillation_from_teacher_alex.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r34_2_arex' \
+                                      --img_root '/content' \
+                                      --note 'of31_A2D' \
+                                      --t_arch 'resnet34' \
+                                      --t-bottleneck-dim 512 \
+                                      --t-model-param '/content/drive/MyDrive/results/PT/MCC/r34/of31_A2D' \
+                                      -d Office31 \
+                                      -s A \
+                                      -t D \
+                                      --epochs 20 \
+                                      -i 500 \
+                                      --seed 2 \
+                                      --mcc_temp 2.5 \
+                                      --st_temp 4.0     
+# D→A
+CUDA_VISIBLE_DEVICES=0 python3 Distillation_from_teacher_alex.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r34_2_arex' \
+                                      --img_root '/content' \
+                                      --note 'of31_D2A' \
+                                      --t_arch 'resnet34' \
+                                      --t-bottleneck-dim 512 \
+                                      --t-model-param '/content/drive/MyDrive/results/PT/MCC/r34/of31_D2A' \
+                                      -d Office31 \
+                                      -s D \
+                                      -t A \
+                                      --epochs 20 \
+                                      -i 500 \
+                                      --seed 2 \
+                                      --mcc_temp 2.5 \
+                                      --st_temp 4.0
+# W→A
+CUDA_VISIBLE_DEVICES=0 python3 Distillation_from_teacher_alex.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r34_2_arex' \
+                                      --img_root '/content' \
+                                      --note 'of31_W2A' \
+                                      --t_arch 'resnet34' \
+                                      --t-bottleneck-dim 512 \
+                                      --t-model-param '/content/drive/MyDrive/results/PT/MCC/r34/of31_W2A' \
+                                      -d Office31 \
+                                      -s W \
+                                      -t A \
+                                      --epochs 20 \
+                                      -i 500 \
+                                      --seed 2 \
+                                      --mcc_temp 2.5 \
+                                      --st_temp 4.0                                        
