62a1723a00df8c17c0f983c93f711bb941637f36
山名　泰生 <54575368+TaiseiYamana@users.noreply.github.com>
2021-07-04 22:51:39 +0900

    Update train_base.sh

--- source/train_MobileDA/train_base.sh
+++ source/train_MobileDA/train_base.sh
@@ -10,25 +10,14 @@ CUDA_VISIBLE_DEVICES=0 python3 train_base.py --save_root '/content/drive/MyDrive
                                       --epochs 20 \
                                       -i 500 \
                                       --seed 2
-# A→D
+# D→W
 CUDA_VISIBLE_DEVICES=0 python3 train_base.py --save_root '/content/drive/MyDrive/results/Base/r34' \
                                       --img_root '/content' \
-                                      --note 'of31_A2D' \
+                                      --note 'of31_D2W' \
                                       -a 'resnet34' \
                                       -d Office31 \
-                                      -s A \
-                                      -t D \
-                                      --epochs 20 \
-                                      -i 500 \
-                                      --seed 2
-# W→A
-CUDA_VISIBLE_DEVICES=0 python3 train_base.py --save_root '/content/drive/MyDrive/results/Base/r34' \
-                                      --img_root '/content' \
-                                      --note 'of31_W2A' \
-                                      -a 'resnet34' \
-                                      -d Office31 \
-                                      -s W \
-                                      -t A \
+                                      -s D \
+                                      -t W \
                                       --epochs 20 \
                                       -i 500 \
                                       --seed 2
@@ -42,7 +31,18 @@ CUDA_VISIBLE_DEVICES=0 python3 train_base.py --save_root '/content/drive/MyDrive
                                       -t D \
                                       --epochs 20 \
                                       -i 500 \
-                                      --seed 2              
+                                      --seed 2                                       
+# A→D
+CUDA_VISIBLE_DEVICES=0 python3 train_base.py --save_root '/content/drive/MyDrive/results/Base/r34' \
+                                      --img_root '/content' \
+                                      --note 'of31_A2D' \
+                                      -a 'resnet34' \
+                                      -d Office31 \
+                                      -s A \
+                                      -t D \
+                                      --epochs 20 \
+                                      -i 500 \
+                                      --seed 2
 # D→A
 CUDA_VISIBLE_DEVICES=0 python3 train_base.py --save_root '/content/drive/MyDrive/results/Base/r34' \
                                       --img_root '/content' \
@@ -53,15 +53,15 @@ CUDA_VISIBLE_DEVICES=0 python3 train_base.py --save_root '/content/drive/MyDrive
                                       -t A \
                                       --epochs 20 \
                                       -i 500 \
-                                      --seed 2                      
-# D→W
+                                      --seed 2                                        
+# W→A
 CUDA_VISIBLE_DEVICES=0 python3 train_base.py --save_root '/content/drive/MyDrive/results/Base/r34' \
                                       --img_root '/content' \
-                                      --note 'of31_D2W' \
+                                      --note 'of31_W2A' \
                                       -a 'resnet34' \
                                       -d Office31 \
-                                      -s D \
-                                      -t W \
+                                      -s W \
+                                      -t A \
                                       --epochs 20 \
                                       -i 500 \
-                                      --seed 2 
+                                      --seed 2                                 
