d1993f415ced5f2cec075c2a2bc781b7cd227974
山名　泰生 <54575368+TaiseiYamana@users.noreply.github.com>
2021-07-12 00:40:39 +0900

    Update train_mobileda_v2_2_alex.sh

--- source/train_MobileDA_v2/train_mobileda_v2_2_alex.sh
+++ source/train_MobileDA_v2/train_mobileda_v2_2_alex.sh
@@ -1,92 +1,26 @@
 # office-31
 # resnet34→arexnet
 # A→W
-CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_v2_2_alex.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r34_2_arex' \
-                                      --img_root '/content' \
-                                      --note 'of31_A2W' \
-                                      --t_arch 'resnet34' \
-                                      --t-bottleneck-dim 512 \
-                                      --t-model-param '/content/drive/MyDrive/results/PT/MCC/r34/of31_A2W/model_best.pth.tar' \
-                                      -d Office31 \
-                                      -s A \
-                                      -t W \
-                                      --epochs 20 \
-                                      -i 500 \
-                                      --seed 2 \
-                                      --mcc_temp 2.5 \
-                                      --st_temp 4.0
+CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_v2_2_alex.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r50_2_arex/of31' \
+                                                           --img_root '/content' \
+                                                           --note 'A2W' \
+                                                           --t_arch 'resnet50' \
+                                                           --t-model-param '/content/drive/MyDrive/results/PT/MCC/r34/of31_A2W/model_best.pth.tar' \
+                                                           -d Office31 \
+                                                           -s A \
+                                                           -t W \
+                                                           --epochs 20 \
+                                                           -i 500 \
+                                                           --seed 2 \
+                                                           --mcc_temp 2.5 \
+                                                           --st_temp 4.0
 # D→W
-CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_v2_2_alex.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r34_2_arex' \
-                                      --img_root '/content' \
-                                      --note 'of31_D2W' \
-                                      --t_arch 'resnet34' \
-                                      --t-bottleneck-dim 512 \
-                                      --t-model-param '/content/drive/MyDrive/results/PT/MCC/r34/of31_D2W/model_best.pth.tar' \
-                                      -d Office31 \
-                                      -s D \
-                                      -t W \
-                                      --epochs 20 \
-                                      -i 500 \
-                                      --seed 2 \
-                                      --mcc_temp 2.5 \
-                                      --st_temp 4.0 
+ 
 # W→D
-CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_v2_2_alex.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r34_2_arex' \
-                                      --img_root '/content' \
-                                      --note 'of31_W2D' \
-                                      --t_arch 'resnet34' \
-                                      --t-bottleneck-dim 512 \
-                                      --t-model-param '/content/drive/MyDrive/results/PT/MCC/r34/of31_W2D/model_best.pth.tar' \
-                                      -d Office31 \
-                                      -s W \
-                                      -t D \
-                                      --epochs 20 \
-                                      -i 500 \
-                                      --seed 2 \
-                                      --mcc_temp 2.5 \
-                                      --st_temp 4.0      
+     
 # A→D
-CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_v2_2_alex.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r34_2_arex' \
-                                      --img_root '/content' \
-                                      --note 'of31_A2D' \
-                                      --t_arch 'resnet34' \
-                                      --t-bottleneck-dim 512 \
-                                      --t-model-param '/content/drive/MyDrive/results/PT/MCC/r34/of31_A2D/model_best.pth.tar' \
-                                      -d Office31 \
-                                      -s A \
-                                      -t D \
-                                      --epochs 20 \
-                                      -i 500 \
-                                      --seed 2 \
-                                      --mcc_temp 2.5 \
-                                      --st_temp 4.0     
+     
 # D→A
-CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_v2_2_alex.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r34_2_arex' \
-                                      --img_root '/content' \
-                                      --note 'of31_D2A' \
-                                      --t_arch 'resnet34' \
-                                      --t-bottleneck-dim 512 \
-                                      --t-model-param '/content/drive/MyDrive/results/PT/MCC/r34/of31_D2A/model_best.pth.tar' \
-                                      -d Office31 \
-                                      -s D \
-                                      -t A \
-                                      --epochs 20 \
-                                      -i 500 \
-                                      --seed 2 \
-                                      --mcc_temp 2.5 \
-                                      --st_temp 4.0
+
 # W→A
-CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_v2_2_alex.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r34_2_arex' \
-                                      --img_root '/content' \
-                                      --note 'of31_W2A' \
-                                      --t_arch 'resnet34' \
-                                      --t-bottleneck-dim 512 \
-                                      --t-model-param '/content/drive/MyDrive/results/PT/MCC/r34/of31_W2A/model_best.pth.tar' \
-                                      -d Office31 \
-                                      -s W \
-                                      -t A \
-                                      --epochs 20 \
-                                      -i 500 \
-                                      --seed 2 \
-                                      --mcc_temp 2.5 \
-                                      --st_temp 4.0                                        
+                                        
