2835a73700d0da64006194041af39ba808dc6518
山名　泰生 <54575368+TaiseiYamana@users.noreply.github.com>
2021-07-12 00:47:23 +0900

    Update train_mobileda_v2_2_alex.py

--- source/train_MobileDA_v2/train_mobileda_v2_2_alex.py
+++ source/train_MobileDA_v2/train_mobileda_v2_2_alex.py
@@ -164,6 +164,10 @@ def main(args):
           			　　　　　　　　　　'prec@1': t_test_top1,
           			　　　　　　　　　　'prec@5': t_test_top5,}, 
 			    	　　　　　　　　　　is_best, args.save_root)
+    # print experiment result
+    checkpoint = torch.load(args.save_root)		
+    logging.info('{}: {}->{} \nTopAcc:{:.2f} ({} epoch)'.format(args.dataset, args.source, args.target, checkpoint['prec@1'], checkpoint['epoch']))			
+			
 
 def train(iters, nets, optimizer, lr_scheduler, cls, mcc, st, epoch, args):
 	batch_time = AverageMeter()
