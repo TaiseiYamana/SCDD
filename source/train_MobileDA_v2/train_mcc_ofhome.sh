887b673233d87b971a638c785cecad31dbc9f8da
山名　泰生 <54575368+TaiseiYamana@users.noreply.github.com>
2021-07-12 01:16:19 +0900

    Create train_mcc_ofhome.sh

---
+++ source/train_MobileDA_v2/train_mcc_ofhome.sh
@@ -0,0 +1,146 @@
+# Office31
+# resnet50
+# Ar→Cl
+CUDA_VISIBLE_DEVICES=0 python3 train_mcc.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r50/ofhome' \
+                                            --img_root '/content' \
+                                            --note 'Ar2Cl' \
+                                            -a 'resnet50' \
+                                            -d Officehome \
+                                            -s Ar \
+                                            -t Cl \
+                                            --epochs 20 \
+                                            -i 500 \
+                                            --seed 2 \
+                                            --temperature 2.5                                           
+# Ar→Pr
+CUDA_VISIBLE_DEVICES=0 python3 train_mcc.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r50/ofhome' \
+                                            --img_root '/content' \
+                                            --note 'Ar2Pr' \
+                                            -a 'resnet50' \
+                                            -d Officehome \
+                                            -s Ar \
+                                            -t Pr \
+                                            --epochs 20 \
+                                            -i 500 \
+                                            --seed 2 \
+                                            --temperature 2.5
+# Ar→Rw
+CUDA_VISIBLE_DEVICES=0 python3 train_mcc.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r50/ofhome' \
+                                            --img_root '/content' \
+                                            --note 'Ar2Rw' \
+                                            -a 'resnet50' \
+                                            -d Officehome \
+                                            -s Ar \
+                                            -t Rw \
+                                            --epochs 20 \
+                                            -i 500 \
+                                            --seed 2 \
+                                            --temperature 2.5
+# Cl→Ar
+CUDA_VISIBLE_DEVICES=0 python3 train_mcc.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r50/ofhome' \
+                                            --img_root '/content' \
+                                            --note 'Cl2Ar' \
+                                            -a 'resnet50' \
+                                            -d Officehome \
+                                            -s Cl \
+                                            -t Ar \
+                                            --epochs 20 \
+                                            -i 500 \
+                                            --seed 2 \
+                                            --temperature 2.5
+# Cl→Pr
+CUDA_VISIBLE_DEVICES=0 python3 train_mcc.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r50/ofhome' \
+                                            --img_root '/content' \
+                                            --note 'Cl2Pr' \
+                                            -a 'resnet50' \
+                                            -d Officehome \
+                                            -s Cl \
+                                            -t Pr \
+                                            --epochs 20 \
+                                            -i 500 \
+                                            --seed 2 \
+                                            --temperature 2.5
+# Cl→Rw
+CUDA_VISIBLE_DEVICES=0 python3 train_mcc.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r50/ofhome' \
+                                            --img_root '/content' \
+                                            --note 'Cl2Rw' \
+                                            -a 'resnet50' \
+                                            -d Officehome \
+                                            -s Cl \
+                                            -t Rw \
+                                            --epochs 20 \
+                                            -i 500 \
+                                            --seed 2 \
+                                            --temperature 2.5
+# Pr→Ar
+CUDA_VISIBLE_DEVICES=0 python3 train_mcc.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r50/ofhome' \
+                                            --img_root '/content' \
+                                            --note 'Pr2Ar' \
+                                            -a 'resnet50' \
+                                            -d Officehome \
+                                            -s Pr \
+                                            -t Ar \
+                                            --epochs 20 \
+                                            -i 500 \
+                                            --seed 2 \
+                                            --temperature 2.5
+# Pr→Cl
+CUDA_VISIBLE_DEVICES=0 python3 train_mcc.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r50/ofhome' \
+                                            --img_root '/content' \
+                                            --note 'Pr2Cl' \
+                                            -a 'resnet50' \
+                                            -d Officehome \
+                                            -s Pr \
+                                            -t Cl \
+                                            --epochs 20 \
+                                            -i 500 \
+                                            --seed 2 \
+                                            --temperature 2.5
+# Pr→Rw
+CUDA_VISIBLE_DEVICES=0 python3 train_mcc.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r50/ofhome' \
+                                            --img_root '/content' \
+                                            --note 'Pr2Rw' \
+                                            -a 'resnet50' \
+                                            -d Officehome \
+                                            -s Pr \
+                                            -t Rw \
+                                            --epochs 20 \
+                                            -i 500 \
+                                            --seed 2 \
+                                            --temperature 2.5
+# Rw→Ar
+CUDA_VISIBLE_DEVICES=0 python3 train_mcc.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r50/ofhome' \
+                                            --img_root '/content' \
+                                            --note 'Rw2Ar' \
+                                            -a 'resnet50' \
+                                            -d Officehome \
+                                            -s Rw \
+                                            -t Ar \
+                                            --epochs 20 \
+                                            -i 500 \
+                                            --seed 2 \
+                                            --temperature 2.5
+# Rw→Cl
+CUDA_VISIBLE_DEVICES=0 python3 train_mcc.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r50/ofhome' \
+                                            --img_root '/content' \
+                                            --note 'Rw2Cl' \
+                                            -a 'resnet50' \
+                                            -d Officehome \
+                                            -s Rw \
+                                            -t Cl \
+                                            --epochs 20 \
+                                            -i 500 \
+                                            --seed 2 \
+                                            --temperature 2.5
+# Rw→Pr
+CUDA_VISIBLE_DEVICES=0 python3 train_mcc.py --save_root '/content/drive/MyDrive/results/MobileDA_v2/r50/ofhome' \
+                                            --img_root '/content' \
+                                            --note 'Rw2Pr' \
+                                            -a 'resnet50' \
+                                            -d Officehome \
+                                            -s Rw \
+                                            -t Pr \
+                                            --epochs 20 \
+                                            -i 500 \
+                                            --seed 2 \
+                                            --temperature 2.5
