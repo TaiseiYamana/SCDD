5175d5e95f6283687bcd41fd2b9ea01f6a09af94
山名　泰生 <54575368+TaiseiYamana@users.noreply.github.com>
2021-08-09 23:53:53 +0900

    Update raytune_mcc_batch.py

--- raytune_mcc_batch.py
+++ raytune_mcc_batch.py
@@ -12,6 +12,7 @@ import torch.nn as nn
 import torch.backends.cudnn as cudnn
 from torch.optim import SGD
 from torch.optim.lr_scheduler import LambdaLR
+from torch.utils.data import DataLoader
 import torchvision.transforms as T
 
 # ray tune
