48d0e9657a0ac4b00cf7ee3dd38a182ebcee7d41
山名　泰生 <54575368+TaiseiYamana@users.noreply.github.com>
2021-08-13 01:02:53 +0900

    Add ImageCELF.py

---
+++ common/vision/datasets/imageclef.py
@@ -0,0 +1,22 @@
+from typing import Optional
+import os
+from .imagelist import ImageList
+from ._util import download as download_data, check_exits
+
+class ImageCLEF(ImageList):
+    image_list = {
+        "C": "image_list/c.txt",
+        "I": "image_list/i.txt",
+        "P": "image_list/p.txt",
+        "B": "image_list/b.txt"
+    }
+    CLASSES = ['aeroplane', 'bike', 'bird', 'boat', 'bottle', 'bus', 'car', 'dog', 'horse', 'monitor', 'motorbike', 'people']
+
+    def __init__(self, root: str, task: str, **kwargs):
+        assert task in self.image_list
+        data_list_file = os.path.join(root, self.image_list[task])
+
+        super(ImageCLEF, self).__init__(root, ImageCLEF.CLASSES, data_list_file=data_list_file, **kwargs)
+    @classmethod
+    def domains(cls):
+        return list(cls.image_list.keys())
