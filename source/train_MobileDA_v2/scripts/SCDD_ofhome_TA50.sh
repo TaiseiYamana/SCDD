816c902d84fce0a13af7b3644a54463c435a74be
山名　泰生 <54575368+TaiseiYamana@users.noreply.github.com>
2021-09-06 16:23:28 +0900

    update

--- source/train_MobileDA_v2/scripts/SCDD_ofhome_TA50.sh
+++ source/train_MobileDA_v2/scripts/SCDD_ofhome_TA50.sh
@@ -26,19 +26,6 @@
                                                            --seed 2 \
                                                            --mcc_temp 2.5 \
                                                            --st_temp 8.0
-!CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_v2.py --save_root '/content/drive/MyDrive/results/SCDD/ofhome/r50_2_r18' \
-                                                           --img_root '/content' \
-                                                           --note 'Ar2Cl' \
-                                                           --t_arch 'resnet50' \
-                                                           --s_arch 'resnet18' \
-                                                           --t-model-param '/content/drive/MyDrive/results/MCC/ofhome/r50/Ar2Cl/model_best.pth.tar' \
-                                                           -d OfficeHome \
-                                                           -s Ar \
-                                                           -t Cl \
-                                                           -i 500 \
-                                                           --seed 2 \
-                                                           --mcc_temp 2.5 \
-                                                           --st_temp 8.0
 # Ar→Pr
 !CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_v2.py --save_root '/content/drive/MyDrive/results/SCDD/ofhome/r50_2_r50' \
                                                            --img_root '/content' \
@@ -59,19 +46,6 @@
                                                            --t_arch 'resnet50' \
                                                            --s_arch 'resnet18' \
                                                            --t-model-param '/content/drive/MyDrive/results/SCDD/ofhome/r50_2_r50/Ar2Pr/model_best.pth.tar' \
-                                                           -d OfficeHome \
-                                                           -s Ar \
-                                                           -t Pr \
-                                                           -i 500 \
-                                                           --seed 2 \
-                                                           --mcc_temp 2.5 \
-                                                           --st_temp 8.0
-!CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_v2.py --save_root '/content/drive/MyDrive/results/SCDD/ofhome/r50_2_r18' \
-                                                           --img_root '/content' \
-                                                           --note 'Ar2Pr' \
-                                                           --t_arch 'resnet50' \
-                                                           --s_arch 'resnet18' \
-                                                           --t-model-param '/content/drive/MyDrive/results/MCC/ofhome/r50/Ar2Pr/model_best.pth.tar' \
                                                            -d OfficeHome \
                                                            -s Ar \
                                                            -t Pr \
@@ -106,19 +80,6 @@
                                                            --seed 2 \
                                                            --mcc_temp 2.5 \
                                                            --st_temp 8.0
-!CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_v2.py --save_root '/content/drive/MyDrive/results/SCDD/ofhome/r50_2_r18' \
-                                                           --img_root '/content' \
-                                                           --note 'Cl2Ar' \
-                                                           --t_arch 'resnet50' \
-                                                           --s_arch 'resnet18' \
-                                                           --t-model-param '/content/drive/MyDrive/results/MCC/ofhome/r50/Cl2Ar/model_best.pth.tar' \
-                                                           -d OfficeHome \
-                                                           -s Cl \
-                                                           -t Ar \
-                                                           -i 500 \
-                                                           --seed 2 \
-                                                           --mcc_temp 2.5 \
-                                                           --st_temp 8.0 
 # Cl→Pr
 !CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_v2.py --save_root '/content/drive/MyDrive/results/SCDD/ofhome/r50_2_r50' \
                                                            --img_root '/content' \
@@ -139,19 +100,6 @@
                                                            --t_arch 'resnet50' \
                                                            --s_arch 'resnet18' \
                                                            --t-model-param '/content/drive/MyDrive/results/SCDD/ofhome/r50_2_r50/Cl2Pr/model_best.pth.tar' \
-                                                           -d OfficeHome \
-                                                           -s Cl \
-                                                           -t Pr \
-                                                           -i 500 \
-                                                           --seed 2 \
-                                                           --mcc_temp 2.5 \
-                                                           --st_temp 8.0
-!CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_v2.py --save_root '/content/drive/MyDrive/results/SCDD/ofhome/r50_2_r18' \
-                                                           --img_root '/content' \
-                                                           --note 'Cl2Pr' \
-                                                           --t_arch 'resnet50' \
-                                                           --s_arch 'resnet18' \
-                                                           --t-model-param '/content/drive/MyDrive/results/MCC/ofhome/r50/Cl2Pr/model_best.pth.tar' \
                                                            -d OfficeHome \
                                                            -s Cl \
                                                            -t Pr \
@@ -186,19 +134,6 @@
                                                            --seed 2 \
                                                            --mcc_temp 2.5 \
                                                            --st_temp 8.0
-!CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_v2.py --save_root '/content/drive/MyDrive/results/SCDD/ofhome/r50_2_r18' \
-                                                           --img_root '/content' \
-                                                           --note 'Pr2Ar' \
-                                                           --t_arch 'resnet50' \
-                                                           --s_arch 'resnet18' \
-                                                           --t-model-param '/content/drive/MyDrive/results/MCC/ofhome/r50/Pr2Ar/model_best.pth.tar' \
-                                                           -d OfficeHome \
-                                                           -s Pr \
-                                                           -t Ar \
-                                                           -i 500 \
-                                                           --seed 2 \
-                                                           --mcc_temp 2.5 \
-                                                           --st_temp 8.0
 # Pr→Cl
 !CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_v2.py --save_root '/content/drive/MyDrive/results/SCDD/ofhome/r50_2_r50' \
                                                            --img_root '/content' \
@@ -225,17 +160,4 @@
                                                            -i 500 \
                                                            --seed 2 \
                                                            --mcc_temp 2.5 \
-                                                           --st_temp 8.0                                                          
-!CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_v2.py --save_root '/content/drive/MyDrive/results/SCDD/ofhome/r50_2_r18' \
-                                                           --img_root '/content' \
-                                                           --note 'Pr2Cl' \
-                                                           --t_arch 'resnet50' \
-                                                           --s_arch 'resnet18' \
-                                                           --t-model-param '/content/drive/MyDrive/results/MCC/ofhome/r50/Pr2Cl/model_best.pth.tar' \
-                                                           -d OfficeHome \
-                                                           -s Pr \
-                                                           -t Cl \
-                                                           -i 500 \
-                                                           --seed 2 \
-                                                           --mcc_temp 2.5 \
-                                                           --st_temp 8.0                                                                                                                                                                           
+                                                           --st_temp 8.0                                                                                                                                                                                                                                    
