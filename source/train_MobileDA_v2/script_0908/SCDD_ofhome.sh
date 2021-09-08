e7a6c54cfd177a5019bdca0ece1c76994a8152b1
山名　泰生 <54575368+TaiseiYamana@users.noreply.github.com>
2021-09-08 17:52:38 +0900

    update

--- source/train_MobileDA_v2/script_0908/SCDD_ofhome.sh
+++ source/train_MobileDA_v2/script_0908/SCDD_ofhome.sh
@@ -1,3 +1,57 @@
+# Ar→Cl
+# 1. pre domain adaptation teacher model
+!CUDA_VISIBLE_DEVICES=0 python3 train_mcc.py --save_root '/content/drive/MyDrive/SCDD/ofhome/r50' \
+                                            --img_root '/content' \
+                                            --note 'Ar2Cl' \
+                                            -a 'resnet50' \
+                                            -d OfficeHome \
+                                            -s Ar \
+                                            -t Cl \
+                                            --epochs 30 \
+                                            --lr 0.01 \
+                                            -i 1000 \
+                                            -b 64 \
+                                            --temperature 2.5 
+# 2. SCDD resnet50→resnet34
+!CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_v2.py --save_root '/content/drive/MyDrive/SCDD/ofhome/r50_2_r34' \
+                                                           --img_root '/content' \
+                                                           --note 'Ar2Cl' \
+                                                           --t_arch 'resnet50' \
+                                                           --s_arch 'resnet34' \
+                                                           --t-model-param '/content/drive/MyDrive/SCDD/ofhome/r50/Ar2Cl/model_best.pth.tar' \
+                                                           -d OfficeHome \
+                                                           -s Ar \
+                                                           -t Cl \
+                                                           -i 500 \
+                                                           --mcc_temp 2.5 \
+                                                           --st_temp 8.0
+# 3. SCDD resnet34→resnet18                                                           
+!CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_v2.py --save_root '/content/drive/MyDrive/SCDD/ofhome/r50_2_r34_2_r18' \
+                                                           --img_root '/content' \
+                                                           --note 'Ar2Cl' \
+                                                           --t_arch 'resnet34' \
+                                                           --s_arch 'resnet18' \
+                                                           --t-model-param '/content/drive/MyDrive/SCDD/ofhome/r50_2_r34/Ar2Cl/model_best.pth.tar' \
+                                                           -d OfficeHome \
+                                                           -s Ar \
+                                                           -t Cl \
+                                                           -i 500 \
+                                                           --seed 2 \
+                                                           --mcc_temp 2.5 \
+                                                           --st_temp 8.0
+# 4. SCDD resnet50→resnet18
+!CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_v2.py --save_root '/content/drive/MyDrive/SCDD/ofhome/r50_2_r18' \
+                                                           --img_root '/content' \
+                                                           --note 'Ar2Cl' \
+                                                           --t_arch 'resnet50' \
+                                                           --s_arch 'resnet18' \
+                                                           --t-model-param '/content/drive/MyDrive/SCDD/ofhome/r50/Ar2Cl/model_best.pth.tar' \
+                                                           -d OfficeHome \
+                                                           -s Ar \
+                                                           -t Cl \
+                                                           -i 500 \
+                                                           --mcc_temp 2.5 \
+                                                           --st_temp 8.0
 # Ar→Pr
 # 1. pre domain adaptation teacher model
 !CUDA_VISIBLE_DEVICES=0 python3 train_mcc.py --save_root '/content/drive/MyDrive/SCDD/ofhome/r50' \
