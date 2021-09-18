e845a41195f0b57ef1f2fd174a873d58a127c98d
山名　泰生 <54575368+TaiseiYamana@users.noreply.github.com>
2021-09-18 21:23:25 +0900

    Update SCDD_ofhome.sh

--- source/train_MobileDA_v2/script_0908/SCDD_ofhome.sh
+++ source/train_MobileDA_v2/script_0908/SCDD_ofhome.sh
@@ -28,4 +28,33 @@
                                                            -b 64 \
                                                            --epochs 200 \
                                                            --mcc_temp 2.5 \
-                                                           --st_temp 8.0                                        
+                                                           --st_temp 8.0 
+!CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_v2.py --save_root '/content/drive/MyDrive/SCDD/ofhome/r50_2_r34_2_r18' \
+                                                           --img_root '/content' \
+                                                           --note 'Ar2Cr' \
+                                                           --t_arch 'resnet34' \
+                                                           --s_arch 'resnet18' \
+                                                           --t-model-param '/content/drive/MyDrive/SCDD/ofhome/r50_r34/Ar2Cl/model_best.pth.tar' \
+                                                           -d OfficeHome \
+                                                           -s Ar \
+                                                           -t Cl \
+                                                           -i 500 \
+                                                           -b 64 \
+                                                           --epochs 200 \
+                                                           --mcc_temp 2.5 \
+                                                           --st_temp 8.0
+!CUDA_VISIBLE_DEVICES=0 python3 train_mobileda_v2.py --save_root '/content/drive/MyDrive/SCDD/ofhome/r50_2_r18' \
+                                                           --img_root '/content' \
+                                                           --note 'Ar2Cr' \
+                                                           --t_arch 'resnet50' \
+                                                           --s_arch 'resnet18' \
+                                                           --t-model-param '/content/drive/MyDrive/SCDD/ofhome/r50/Ar2Cl/model_best.pth.tar' \
+                                                           -d OfficeHome \
+                                                           -s Ar \
+                                                           -t Cl \
+                                                           -i 500 \
+                                                           -b 64 \
+                                                           --epochs 200 \
+                                                           --mcc_temp 2.5 \
+                                                           --st_temp 8.0                                                            
+                                                           
