c41ea676b069ac27fca53930c3a6586b4e532157
TaiseiYamana <mit.yamatail@gmail.com>
2021-09-30 03:41:18 +0000

    update

--- source/train_MobileDA_v2/train_dcoral.py
+++ source/train_MobileDA_v2/train_dcoral.py
@@ -283,11 +283,11 @@ def train(iters, net, optimizer, lr_scheduler, cls, dcoral, epoch, args):
 					   'Time:{batch_time.val:.4f}'
 					   'Data:{data_time.val:.4f}'
 					   'Cls:{cls_losses.val:.4f}({cls_losses.avg:.4f})'
-					   'MCC:{mcc_losses.val:.4f}({mcc_losses.avg:.4f})'
+					   'DCORAL:{dcoral_losses.val:.4f}({mcc_losses.avg:.4f})'
 					   'prec@1:{top1.val:.2f}({top1.avg:.2f})'
 					   'prec@5:{top5.val:.2f}({top5.avg:.2f})'.format(
 					   epoch, i, args.iters_per_epoch, batch_time=batch_time, data_time=data_time,
-					   cls_losses=cls_losses, mcc_losses=mcc_losses, top1=top1, top5=top5))
+					   cls_losses=cls_losses, dcoral_losses=dcoral_losses, top1=top1, top5=top5))
 			logging.info(log_str)
 
 
