200d90b535920e76e34534f7573182168de9b48c
山名　泰生 <54575368+TaiseiYamana@users.noreply.github.com>
2021-10-22 01:51:14 +0900

    update

--- common/vision/models/mobilenet.py
+++ common/vision/models/mobilenet.py
@@ -19,6 +19,8 @@ class MobileNetV3(models.MobileNetV3):
         cls_in_features = self.classifier[0].in_features
         self._out_features = self.classifier[3].in_features
 
+        print(args)
+
         self.classifier = nn.Sequential(
             nn.Linear(cls_in_features, self._out_features),
             nn.Hardswish(inplace=True),
