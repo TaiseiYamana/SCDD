fa105749ca774a3af2df05308af9875c7ce13570
TaiseiYamana <mit.yamatail@gmail.com>
2022-02-11 20:59:44 +0900

    update

--- test.py
+++
@@ -1,9 +0,0 @@
-import models
-
-architecture_names = sorted(
-        name for name in models.__dict__
-        if name.islower() and not name.startswith("__")
-        and callable(models.__dict__[name]))
-
-
-print('\n'.join(architecture_names))
