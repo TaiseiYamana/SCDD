1d05a4aad8156d1577a8f4fc2f0b26528130c9ef
TaiseiYamana <mit.yamatail@gmail.com>
2022-02-14 05:43:07 +0900

    update

--- source/analysis.py
+++ source/analysis.py
@@ -178,8 +178,8 @@ def main(args):
     cm_list = test(target_test_loader, net, cls, mcc,  args.t + 'Domain')
     cm_pd = ConfusionMatrix(actual_vector=cm_list.t, predict_vector=cm_list.y)
     cm_pd.classes = target_test_dataset.CLASSES
-    plt.figure()
-    plt.tight_layout()
+    #plt.figure()
+    plt.figure(figsize=(100, 100), dpi=100)
     confusion_matrix.plot_cm(cm_pd, annot=True)
     logging.info("Saving Confusion Matrix to {}".format(tSNE_filename))
     plt.savefig(CM_filename, bbox_inches='tight')
