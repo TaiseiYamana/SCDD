a56413f18775717065ef050ccab46bf1902c39ed
TaiseiYamana <mit.yamatail@gmail.com>
2022-02-14 22:44:31 +0900

    update

---
+++ source/scripts/of31/mcc.sh
@@ -0,0 +1,19 @@
+# Office31
+# A2W
+CUDA_VISIBLE_DEVICES=0 python3 mcc.py --save_root '/content/drive/MyDrive/SCDD/of31/alexnet/mcc' \
+--img_root '/content' \
+--note 'A2W' \
+--arch 'alexnet' \
+-d Office31 \
+-s A \
+-t W \
+-b 64 \
+-i 500 \
+--temperature 2. \
+--trade_off 1.
+
+# D2W
+# W2D
+# A2D
+# D2A
+# W2A
---
+++ source/scripts/of31/source_only.sh
@@ -0,0 +1,17 @@
+# Office31
+# A2W
+CUDA_VISIBLE_DEVICES=0 python3 source_only.py --save_root '/content/drive/MyDrive/SCDD/of31/alex/source_only' \
+--img_root '/content' \
+--note 'A2W' \
+--arch 'resnet50' \
+-d Office31 \
+-s A \
+-t W \
+-b 64 \
+-i 500
+
+# D2W
+# W2D
+# A2D
+# D2A
+# W2A
