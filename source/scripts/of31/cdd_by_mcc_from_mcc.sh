12a6c4b1ed737d5d69b7c64a37c67bc7f01088aa
TaiseiYamana <mit.yamatail@gmail.com>
2022-02-14 19:11:37 +0900

    update

--- source/scripts/of31/cdd_by_mcc_from_mcc.sh
+++ source/scripts/of31/cdd_by_mcc_from_mcc.sh
@@ -1,7 +1,7 @@
 # Office31
 # A2W
 # pretrain teacher
-CUDA_VISIBLE_DEVICES=0 python3 source/mcc.py --save_root '/content/drive/MyDrive/SCDD/of31/r50/mcc' \
+CUDA_VISIBLE_DEVICES=0 python3 mcc.py --save_root '/content/drive/MyDrive/SCDD/of31/r50/mcc' \
 --img_root '/content' \
 --note 'A2W' \
 --arch 'resnet50' \
@@ -12,7 +12,7 @@ CUDA_VISIBLE_DEVICES=0 python3 source/mcc.py --save_root '/content/drive/MyDrive
 --temperature 2. \
 --trade_off 1.
 
-CUDA_VISIBLE_DEVICES=0 python3 source/cdd_with_mcc.py --save_root '/content/drive/MyDrive/SCDD/of31/r50_2_alex/from_mcc' \
+CUDA_VISIBLE_DEVICES=0 python3 cdd_with_mcc.py --save_root '/content/drive/MyDrive/SCDD/of31/r50_2_alex/from_mcc' \
 --img_root '/content' \
 --note 'A2W' \
 --t_arch 'ressnet50' \
--- source/scripts/of31/cdd_by_mcc_from_source_only.sh
+++ source/scripts/of31/cdd_by_mcc_from_source_only.sh
@@ -1,7 +1,7 @@
 # Office31
 # A2W
 # pretrain teacher
-CUDA_VISIBLE_DEVICES=0 python3 source/source_only.py --save_root '/content/drive/MyDrive/SCDD/of31/r50/source_only' \
+CUDA_VISIBLE_DEVICES=0 python3 source_only.py --save_root '/content/drive/MyDrive/SCDD/of31/r50/source_only' \
 --img_root '/content' \
 --note 'A2W' \
 --arch 'resnet50' \
@@ -10,7 +10,7 @@ CUDA_VISIBLE_DEVICES=0 python3 source/source_only.py --save_root '/content/drive
 -t W \
 -b 64
 
-CUDA_VISIBLE_DEVICES=0 python3 source/cdd_with_mcc.py --save_root '/content/drive/MyDrive/SCDD/of31/r50_2_alex/from_mcc' \
+CUDA_VISIBLE_DEVICES=0 python3 cdd_with_mcc.py --save_root '/content/drive/MyDrive/SCDD/of31/r50_2_alex/from_mcc' \
 --img_root '/content' \
 --note 'A2W' \
 --t_arch 'ressnet50' \
