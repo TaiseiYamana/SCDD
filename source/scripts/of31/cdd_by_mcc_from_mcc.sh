cd16d2f3b9883a47301bb6046a0ebc46c845b98e
TaiseiYamana <mit.yamatail@gmail.com>
2022-02-14 19:14:11 +0900

    update

--- source/scripts/of31/cdd_by_mcc_from_mcc.sh
+++ source/scripts/of31/cdd_by_mcc_from_mcc.sh
@@ -15,7 +15,7 @@ CUDA_VISIBLE_DEVICES=0 python3 mcc.py --save_root '/content/drive/MyDrive/SCDD/o
 CUDA_VISIBLE_DEVICES=0 python3 cdd_with_mcc.py --save_root '/content/drive/MyDrive/SCDD/of31/r50_2_alex/from_mcc' \
 --img_root '/content' \
 --note 'A2W' \
---t_arch 'ressnet50' \
+--t_arch 'resnet50' \
 --s_arch 'alexnet' \
 --t_model_param '/content/drive/MyDrive/SCDD/of31/r50/mcc/A2W/model_best.pth.tar' \
 -d Office31 \
