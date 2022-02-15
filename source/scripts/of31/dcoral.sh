ff227bc213796de3c319584a1a966b976e831b9e
TaiseiYamana <mit.yamatail@gmail.com>
2022-02-16 04:04:55 +0900

    update

---
+++ source/scripts/of31/dcoral.sh
@@ -0,0 +1,18 @@
+# Office31
+# A2W
+CUDA_VISIBLE_DEVICES=0 python3 dcoral.py --save_root '/content/drive/MyDrive/SCDD/of31/alexnet/dcoral' \
+--img_root '/content' \
+--note 'A2W' \
+--arch 'alexnet' \
+-d Office31 \
+-s A \
+-t W \
+-i 500 \
+--temperature 2. \
+--trade_off 0.75
+
+# D2W
+# W2D
+# A2D
+# D2A
+# W2A
