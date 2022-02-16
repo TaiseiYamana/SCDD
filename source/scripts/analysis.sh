c3a8f3da23e33e2dbc01157074ecd7f019644627
TaiseiYamana <mit.yamatail@gmail.com>
2022-02-16 12:03:17 +0900

    update

--- source/scripts/analysis.sh
+++ source/scripts/analysis.sh
@@ -1,13 +1,23 @@
 # Office31
 # A2W
-CUDA_VISIBLE_DEVICES=0 python3 analysis.py --save_root '/content/drive/MyDrive/SCDD/of31/SCDD/r50_2_alex/from_mcc/A2W' \
+CUDA_VISIBLE_DEVICES=0 python3 analysis.py --save_root '/content/drive/MyDrive/SCDD/of31/analysis' \
 --img_root '/content' \
---note 'A2W' \
+--note 'A2W/source_only' \
 --arch 'alexnet' \
---model_param '/content/drive/MyDrive/SCDD/of31/SCDD/r50_2_alex/from_mcc/A2W/model_best.pth.tar' \
+--model_param '/content/drive/MyDrive/SCDD/of31/alex/source_only/A2W/model_best.pth.tar' \
 -d Office31 \
 -s A \
 -t W
+
+CUDA_VISIBLE_DEVICES=0 python3 analysis.py --save_root '/content/drive/MyDrive/SCDD/of31/analysis' \
+--img_root '/content' \
+--note 'A2W/cdd_with_mcc_from_mcc' \
+--arch 'alexnet' \
+--model_param '/content/drive/MyDrive/SCDD/of31/r50_2_alex/from_mcc/A2W/model_best.pth.tar' \
+-d Office31 \
+-s A \
+-t W
+
 # D2W
 # W2D
 # A2D
@@ -16,14 +26,6 @@ CUDA_VISIBLE_DEVICES=0 python3 analysis.py --save_root '/content/drive/MyDrive/S
 
 # OfficeHome
 # Ar2Cl
-CUDA_VISIBLE_DEVICES=0 python3 analysis.py --save_root '/content/drive/MyDrive/SCDD/of31/SCDD/r50_2_alex/from_mcc/A2W/' \
---img_root '/content' \
---note 'Ar2Cl' \
---arch 'alexnet' \
---model_param '/content/drive/MyDrive/SCDD/of31/SCDD/r50_2_alex/from_mcc/A2W/model_best.pth.tar' \
--d Office31 \
--s Ar \
--t Cl
 # Ar2Pr
 # Ar2Rw
 # Cl2Ar
